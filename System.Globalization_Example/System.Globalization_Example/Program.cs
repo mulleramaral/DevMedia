4b69163d82fc83a77ca4998b93238d4a0c344456
Miiller caetano do amaral <miilleramaral@gmail.com>
2017-12-19 23:12:30 -0200

    Calendar e CultureInfo

---
+++ System.Globalization_Example/System.Globalization_Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace System.Globalization_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Calendar
+            Calendar cal;
+            DateTime dt = new DateTime(2012, 1, 1, new GregorianCalendar());
+            #endregion
+
+            #region CultureInfo
+            CultureInfo cultureInfo = new CultureInfo("pt-BR");
+
+            #endregion
+        }
+    }
+}
