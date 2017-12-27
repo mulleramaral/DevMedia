23514444a5e29dafb8214db6f42ef445e231e4c8
Muller caetano Desktop <miilleramaral@gmail.com>
2017-12-26 23:00:22 -0200

    21 - System.XML Persistindo dados serializados

--- Framework/System.XML_Example/Form3.Designer.cs
+++ Framework/System.XML_Example/Form3.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContatos = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(12, 100);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 10;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(12, 74);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 9;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 23);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Telefone";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome";
+            // 
+            // lblContatos
+            // 
+            this.lblContatos.AutoSize = true;
+            this.lblContatos.Location = new System.Drawing.Point(162, 10);
+            this.lblContatos.Name = "lblContatos";
+            this.lblContatos.Size = new System.Drawing.Size(0, 13);
+            this.lblContatos.TabIndex = 11;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(464, 316);
+            this.Controls.Add(this.lblContatos);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Windows.Forms.Button btnSalvar;
+        private Windows.Forms.TextBox txtTelefone;
+        private Windows.Forms.TextBox txtNome;
+        private Windows.Forms.Label label2;
+        private Windows.Forms.Label label1;
+        private Windows.Forms.Label lblContatos;
     }
 }
--- Framework/System.XML_Example/Form3.cs
+++ Framework/System.XML_Example/Form3.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.IO;
+using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -8,22 +9,50 @@ namespace System.XML_Example
     {
         string arquivo = @"C:\Temp\agenda.xml";
         XmlDocument xmlDoc = new XmlDocument();
+        XElement xDoc;
+        Contatos contatos;
+        
 
         public Form3()
         {
             InitializeComponent();
+            if (!File.Exists(arquivo))
+            {
+                XmlNode nodeRoot = xmlDoc.CreateElement("Contatos");
+                xmlDoc.AppendChild(nodeRoot);
+                xmlDoc.Save(arquivo);
+            }
+        }
+
+        private void ReadAgenda()
+        {
+            xDoc = XElement.Load(arquivo);
+            contatos = Serializer.Deserialize<Contatos>(xDoc);
+            lblContatos.Text = string.Empty;
+
+            foreach (Contato c in contatos.Contato)
+            {
+                lblContatos.Text += "Nome: " + c.Nome + "\nTelefone: " + c.Telefone + "\n\n";
+            }
         }
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            //Deserialize
-            XElement xElement = XElement.Load(arquivo);
-            Contatos contatos = Serializer.Deserialize<Contatos>(xElement);
-
-            //Serialize
-
-            XElement xmlReturn = Serializer.Serialize<Contatos>(contatos);
+            ReadAgenda();
         }
 
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Contato c = new Contato();
+            c.Nome = txtNome.Text;
+            c.Telefone = txtTelefone.Text;
+
+            contatos.Contato.Add(c);
+
+            XElement xmlReturn = Serializer.Serialize(contatos);
+            xmlReturn.Save(arquivo);
+
+            ReadAgenda();
+        }
     }
 }
