fbea1649ec28aaa101cf9f1f6372f109488e85d5
miilleramaral <miilleramaral@gmail.com>
2018-02-06 23:03:30 -0200

    Crud MVC

---
+++ CrudMvc/CrudMvc/Models/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudMvc.Models
+{
+    [Table("Aluno")]
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        [Display(Name = "Data de Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}",ApplyFormatInEditMode = true)]
+        public DateTime Nascimento { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
---
+++ CrudMvc/CrudMvc/Models/EscolaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace CrudMvc.Models
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext(): base("Escola")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<Aluno> Aluno { get; set; }
+    }
+}
