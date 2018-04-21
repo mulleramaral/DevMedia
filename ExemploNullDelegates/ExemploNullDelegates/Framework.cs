cfad498519b6009a09936caad74760b27ed9eef1
miilleramaral <miilleramaral@gmail.com>
2018-04-21 00:13:32 -0300

    Base criada

---
+++ ExemploNullDelegates/ExemploNullDelegates/Framework.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace ExemploNullDelegates
+{
+    public class Boleto
+    {
+        public void Pagar(double valor)
+        {
+            WriteLine($"Pago valor de {valor} no boleto");
+        }
+    }
+
+    public delegate void PagarEvent (double valor);
+    public class Pedido
+    {
+        public event PagarEvent Pagar;
+
+        //Boleto boleto = new Boleto();
+        public void Fechar(double valor)
+        {
+            //Delegação
+            //boleto.Pagar(valor);
+
+            //Delegação com delegate / event
+            Pagar?.Invoke(valor);
+        }
+    }
+}
---
+++ ExemploNullDelegates/ExemploNullDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using static System.Convert;
+
+namespace ExemploNullDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Informe valor do pedido: ");
+            var valor = ToDouble(ReadLine());
+            var pedido = new Pedido();
+            pedido.Pagar += v => WriteLine($"Pago valor de {v} no boleto");
+            pedido.Fechar(valor);
+            ReadLine();
+        }
+
+        //private static void Pedido_Pagar(double valor)
+        //{
+        //    new Boleto().Pagar(valor);
+        //}
+    }
+}
